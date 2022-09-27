cc349b4460677885851513eaef986ad069dd1927
unknown <msyahruramadhan10@gmail.com>
2022-09-27 11:04:20 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/pembuatanprogram/canteen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pembuatanprogram.canteen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:canteen/launcher.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LauncherPage(),
+    );
+  }
+}
